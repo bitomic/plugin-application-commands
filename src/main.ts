7b803b715c51337b0ad531a876c0610e19a18155
bitomic <35199700+bitomic@users.noreply.github.com>
2022-02-27 14:35:38 -0600

    docs: add comments to document module augments

--- src/main.ts
+++ src/main.ts
@@ -18,24 +18,57 @@ type ApplicationCommandOptions<T extends BaseApplicationCommandData> = Applicati
 
 declare module '@sapphire/framework' {
 	interface SapphireClientOptions {
+		/**
+		 * @description For each application command, the function will be called to get its `guildIds` and/or `idHints`.
+		 *
+		 * @param name Name of the command piece, **not** the application command name.
+		 * @param type Type of application command.
+		 *
+		 * @since 1.1.0
+		 */
 		applicationCommandsHintProvider?: ( name: string, type: 'chatInput' | 'message' | 'user' ) => Awaitable<ApplicationCommandAdditionalData | undefined | null>
 	}
 
 	class Command {
+		/**
+		 * @since 1.0.0
+		 */
 		public chatInputApplicationRun?( interaction: CommandInteraction ): void | Promise<void>
+
+		/**
+		 * @since 1.0.0
+		 */
 		public messageApplicationRun?( interaction: MessageInteraction ): void | Promise<void>
+
+		/**
+		 * @since 1.0.0
+		 */
 		public userApplicationRun?( interaction: UserContextMenuInteraction ): void | Promise<void>
 	}
 
 	interface CommandOptions {
+		/**
+		 * @since 1.0.0
+		 */
 		chatInputApplicationOptions?: ApplicationCommandOptions<ChatInputApplicationCommandData>
+
+		/**
+		 * @since 1.0.0
+		 */
 		messageApplicationOptions?: ApplicationCommandOptions<MessageApplicationCommandData>
+
+		/**
+		 * @since 1.0.0
+		 */
 		userApplicationOptions?: ApplicationCommandOptions<UserApplicationCommandData>
 	}
 }
 
 declare module '@sapphire/pieces' {
 	interface Container {
+		/**
+		 * @since 1.0.0
+		 */
 		applicationCommands: Map<string, ApplicationCommand>
 	}
 }
